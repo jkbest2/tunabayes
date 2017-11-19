40f32dae876940c61ce5929162ad26c7b45e47f9
John Best <jkbest@gmail.com>
2017-11-19 12:17:38 -0800

    Andre's Schaefer model code.

--- Lecture1c.R
+++ Lecture1c.R
@@ -19,7 +19,7 @@ inits2 <- list(r=0.5, K=300, iq=0.8, isigma2=200, itau2=200, P=c(0.99,0.99,0.99,
 inits <- list(inits1,inits2)
 
 fit <- stan(file = 'Schaefer.stan', data = tuna_data, 
-              iter = 50000, chains = 2,init=inits, warmup=5000,verbose=F)
+              iter = 4000, chains = 2,init=inits, warmup=2000,verbose=F)
 print(fit)
 la <- extract(fit, permuted = TRUE) # return a list of arrays 
 
@@ -43,4 +43,39 @@ lines(1:N,quants[3,],lwd=1,lty=2)
 
 
 
+init_f <- function() {
+    list(K = rlnorm(1, 5.0429, 0.5162),
+         r = rlnorm(1, -1.38, 0.51),
+         tau2 = 1 / rgamma(1, 1.709, 0.00861),
+         sigma2 = 1 / rgamma(1, 3.785, 0.0102),
+         q = rexp(1),
+         P = rlnorm(23, 0, 0.1))
+}
 
+color_scheme_set("brightblue")
+mcmc_parcoord(draws, alpha = 0.05)
+mcmc_parcoord(draws, np = np)
+# customize appearance of divergences
+color_scheme_set("darkgray")
+div_style <- parcoord_style_np(div_color = "green", div_size = 0.05, div_alpha = 0.4)
+mcmc_parcoord(draws, size = 0.25, alpha = 0.1,
+np = np, np_style = div_style)
+# to use a transformation (e.g., to standarde all the variables)
+# specify the 'transformations' argument (though partial argument name
+# matching means we can just use 'trans' or 'transform')
+
+library(bayesplot)
+
+fit <- stan('tuna_centered.stan', data = tuna_data, iter = 1e4, init = init_f)
+draws <- as.array(fit)
+np <- nuts_params(fit)
+
+mcmc_parcoord(
+draws,
+pars = c('r', 'K', 'sigma', 'tau'),
+transform = function(x) {(x - mean(x)) / sd(x)},
+size = 0.25,
+alpha = 0.1,
+np = np,
+np_style = div_style
+)

58fffafc60ad16cda31e7acb6e9348c5b0306366
John Best <jkbest@gmail.com>
2017-12-13 13:01:26 -0800

    Just use `stan` to fit.
    - Can access warmup/sampling times from object.
    - Also save each fit individually.

--- src/04_fitmods.R
+++ src/04_fitmods.R
@@ -4,68 +4,43 @@ source('src/02_fitfns.R')
 n_iter <- 1e4
 n_chain <- 4
 
-res0 <- fitmod('src/models/0_Schaefer_T.stan',
-               data = tuna_data,
-               iter = n_iter, chains = n_chain,
-               cores = 1L)
-fit0 <- resT$fit
-time0 <- resT$time
-rm(res0)
+fit0 <- stan('src/models/0_Schaefer_T.stan',
+             data = tuna_data,
+             iter = n_iter, chains = n_chain)
+saveRDS(fit0, 'results/fit0.Rds')
 
-res1 <- fitmod('src/models/1_Schaefer_C.stan',
-               data = tuna_data,
-               iter = n_iter, chains = n_chain,
-               cores = 1L)
-fit1 <- res1$fit
-time1 <- res1$time
-rm(res1)
+fit1 <- stan('src/models/1_Schaefer_C.stan',
+             data = tuna_data,
+             iter = n_iter, chains = n_chain)
+saveRDS(fit1, 'results/fit1.Rds')
 
-# res2 <- fitmod('src/models/2_Schaefer_OL.stan',
+# fit2 <- stan('src/models/2_Schaefer_OL.stan',
 #                data = tuna_data,
-#                iter = n_iter, chains = n_chain,
-#                cores = 1L)
-# fit2 <- res2$fit
-# time2 <- res2$time
-# rm(res2)
+#                iter = n_iter, chains = n_chain)
+# saveRDS(fit2, 'results/fit2.Rds')
 
-res3 <- fitmod('src/models/3_Schaefer_PL.stan',
+fit3 <- stan('src/models/3_Schaefer_PL.stan',
                data = tuna_data,
-               iter = n_iter, chains = n_chain,
-               cores = 1L)
-fit3 <- res3$fit
-time3 <- res3$time
-rm(res3)
+               iter = n_iter, chains = n_chain)
+saveRDS(fit3, 'results/fit3.Rds')
 
-# res4 <- fitmod('src/models/4_Schaefer_OPL.stan',
+# fit4 <- stan('src/models/4_Schaefer_OPL.stan',
 #                data = tuna_data,
-#                iter = n_iter, chains = n_chain,
-#                cores = 1L)
-# fit4 <- res4$fit
-# time4 <- res4$time
-# rm(res4)
+#                iter = n_iter, chains = n_chain)
+# saveRDS(fit4, 'results/fit4.Rds')
 
-# res5 <- fitmod('src/models/5_Schaefer_ON.stan',
+# fit5 <- stan('src/models/5_Schaefer_ON.stan',
 #                data = tuna_data,
-#                iter = n_iter, chains = 5,
-#                cores = 1L)
-# fit5 <- res5$fit
-# time5 <- res5$time
-# rm(res5)
+#                iter = n_iter, chains = 5)
+# saveRDS(fit5, 'results/fit5.Rds')
 
-res6 <- fitmod('src/models/6_Schaefer_PN.stan',
+fit6 <- stan('src/models/6_Schaefer_PN.stan',
                data = tuna_data,
-               iter = n_iter, chains = n_chain,
-               cores = 1L)
-fit6 <- res4$fit
-time6 <- res4$time
-rm(res6)
+               iter = n_iter, chains = n_chain)
+saveRDS(fit6, 'results/fit6.Rds')
 
-# res7 <- fitmod('src/models/7_Schaefer_OPN.stan',
+# fit7 <- stan('src/models/7_Schaefer_OPN.stan',
 #                data = tuna_data,
-#                iter = n_iter, chains = n_chain,
-#                cores = 1L)
-# fit7 <- res7$fit
-# time7 <- res7$time
-# rm(res7)
+#                iter = n_iter, chains = n_chain)
+# saveRDS(fit7, 'results/fit7.Rds')
 
-save.image('results/fit_results.Rdata')

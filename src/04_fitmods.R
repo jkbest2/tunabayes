fcfe367ebe008af450d72235cfc664bc3a71c47d
John Best <jkbest@gmail.com>
2018-11-12 14:39:08 -0800

    Move model fitting to tidy framework

--- src/04_fitmods.R
+++ src/04_fitmods.R
@@ -1,65 +1,38 @@
-source('src/01_setup.R')
+source("src/01_setup.R")
 
 set.seed(38493)
 
-n_iter <- 1e4
-n_chain <- 4
+chain_spec <- list(n_iter = 1e4,
+                   n_warm = 5e3,
+                   n_chain = 4)
 
-fit0 <- stan('src/models/0_Schaefer_T.stan',
-             data = tuna_data,
-             iter = n_iter, chains = n_chain)
-fit0@stanmodel@dso <- new('cxxdso')
-saveRDS(fit0, 'results/fit0.Rds')
+fitmod <- function(file, control) {
+  stan(file = file,
+       data = tuna_data,
+       chains = chain_spec$n_chain,
+       iter = chain_spec$n_iter,
+       warmup = chain_spec$n_warm,
+       control = control)
+}
 
-fit1 <- stan('src/models/1_Schaefer_C.stan',
-             data = tuna_data,
-             iter = n_iter, chains = n_chain)
-# fit1@stanmodel@dso <- new('cxxdso')
-# saveRDS(fit1, 'results/fit1.Rds')
+adj_control = list(adapt_delta = 0.975)
 
-fit1a <- stan('src/models/1_Schaefer_C.stan',
-              data = tuna_data,
-              iter = n_iter, chains = n_chain,
-              control = list(adapt_delta = 0.975))
+fit_df <- tribble(~ model_name,        ~ model_file,   ~ control,
+                   "Truncated",      "0_Schaefer_T",        NULL,
+                    "Centered",      "1_Schaefer_C",        NULL,
+           "Centered Adjusted",      "1_Schaefer_C", adj_control,
+       "Noncentered Lognormal",     "3_Schaefer_PL",        NULL,
+   "Noncentered Lognormal Adj",     "3_Schaefer_PL", adj_control,
+          "Noncentered Normal",     "6_Schaefer_PN",        NULL,
+      "Noncentered Normal Adj",     "6_Schaefer_PN", adj_control,
+              "Marginalized q", "7_Schaefer_marg_q",        NULL,
+          "Marginalized q Adj", "7_Schaefer_marg_q", adj_control) %>%
+  mutate(model_path = paste0("src/models/", model_file, ".stan"),
+         fit = map2(model_path, control, fitmod))
+## Pretty sure this is necessary to save `stanfit` objects so that you don't get
+## weird errors when they are read back into R
+walk(fit_df$fit, function(f) f@stanmodel@dso <- new('cxxdso'))
 
-fit1@stanmodel@dso <- new('cxxdso')
-saveRDS(fit1, 'results/fit1.Rds')
-fit1a@stanmodel@dso <- new('cxxdso')
-saveRDS(fit1a, 'results/fit1a.Rds')
-
-# fit2 <- stan('src/models/2_Schaefer_OL.stan',
-#                data = tuna_data,
-#                iter = n_iter, chains = n_chain)
-# fit2@stanmodel@dso <- new('cxxdso')
-# saveRDS(fit2, 'results/fit2.Rds')
-
-fit3 <- stan('src/models/3_Schaefer_PL.stan',
-               data = tuna_data,
-               iter = n_iter, chains = n_chain)
-fit3@stanmodel@dso <- new('cxxdso')
-saveRDS(fit3, 'results/fit3.Rds')
-
-# fit4 <- stan('src/models/4_Schaefer_OPL.stan',
-#                data = tuna_data,
-#                iter = n_iter, chains = n_chain)
-# fit4@stanmodel@dso <- new('cxxdso')
-# saveRDS(fit4, 'results/fit4.Rds')
-
-# fit5 <- stan('src/models/5_Schaefer_ON.stan',
-#                data = tuna_data,
-#                iter = n_iter, chains = 5)
-# fit5@stanmodel@dso <- new('cxxdso')
-# saveRDS(fit5, 'results/fit5.Rds')
-
-fit6 <- stan('src/models/6_Schaefer_PN.stan',
-               data = tuna_data,
-               iter = n_iter, chains = n_chain)
-fit6@stanmodel@dso <- new('cxxdso')
-saveRDS(fit6, 'results/fit6.Rds')
-
-# fit7 <- stan('src/models/7_Schaefer_OPN.stan',
-#                data = tuna_data,
-#                iter = n_iter, chains = n_chain)
-# fit7@stanmodel@dso <- new('cxxdso')
-# saveRDS(fit7, 'results/fit7.Rds')
+save(fit_df, chain_spec,
+     file = paste0("results/", Sys.time(), "_fits.Rdata"))
 

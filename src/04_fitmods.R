782495f47c77e35c561d1fdc9b86cb4a92e928ef
John Best <jkbest@gmail.com>
2018-12-01 13:15:34 -0800

    Move to tidy fitting of Stan models and post-fit analysis

--- src/04_fitmods.R
+++ src/04_fitmods.R
@@ -17,16 +17,22 @@ fitmod <- function(file, control) {
 
 adj_control = list(adapt_delta = 0.975)
 
-fit_df <- tribble(~ model_name,        ~ model_file,   ~ control,
-                   "Truncated",      "0_Schaefer_T",        NULL,
-                    "Centered",      "1_Schaefer_C",        NULL,
-           "Centered Adjusted",      "1_Schaefer_C", adj_control,
-       "Noncentered Lognormal",     "3_Schaefer_PL",        NULL,
-   "Noncentered Lognormal Adj",     "3_Schaefer_PL", adj_control,
-          "Noncentered Normal",     "6_Schaefer_PN",        NULL,
-      "Noncentered Normal Adj",     "6_Schaefer_PN", adj_control,
-              "Marginalized q", "7_Schaefer_marg_q",        NULL,
-          "Marginalized q Adj", "7_Schaefer_marg_q", adj_control) %>%
+mod_df <- tribble(~ model_name,          ~ model_file,   ~ control,
+                   "Truncated",        "0_Schaefer_T",        NULL,
+                    "Centered",        "1_Schaefer_C",        NULL,
+           "Centered Adjusted",        "1_Schaefer_C", adj_control,
+       "Noncentered Lognormal",       "3_Schaefer_PL",        NULL,
+   "Noncentered Lognormal Adj",       "3_Schaefer_PL", adj_control,
+          "Noncentered Normal",       "6_Schaefer_PN",        NULL,
+      "Noncentered Normal Adj",       "6_Schaefer_PN", adj_control,
+              "Marginalized q",   "7_Schaefer_marg_q",        NULL,
+          "Marginalized q Adj",   "7_Schaefer_marg_q", adj_control,
+          "Explicit catch add",    "8_Schaefer_exp_F",        NULL,
+      "Explicit catch add adj",    "8_Schaefer_exp_F", adj_control,
+            "Exp catch nc add", "9_Schaefer_exp_F_nc",        NULL,
+        "Exp catch nc add adj", "9_Schaefer_exp_F_nc", adj_control)
+
+mod_df %>%
   mutate(model_path = paste0("src/models/", model_file, ".stan"),
          fit = map2(model_path, control, fitmod))
 ## Pretty sure this is necessary to save `stanfit` objects so that you don't get
@@ -36,3 +42,23 @@ walk(fit_df$fit, function(f) f@stanmodel@dso <- new('cxxdso'))
 save(fit_df, chain_spec,
      file = paste0("results/", Sys.time(), "_fits.Rdata"))
 
+data_df <- data_frame(T = c(6, 12, 18, 23),
+                      data = rep(list(tuna_data), 4)) %>%
+  mutate(C = map2(data, T, ~ .x$C[1:.y]),
+         I = map2(data, T, ~ .x$I[1:.y]),
+         data = pmap(list(C, I, T),
+                     ~ list(C = ..1, I = ..2, T = ..3))) %>%
+  select(T, data)
+
+short_df <- cross_df(list(model_name = fit_df$model_name,
+                          T = c(6, 12, 18, 23))) %>%
+  left_join(data_df, by = "T") %>%
+  left_join(mod_df, by = "model_name") %>%
+  mutate(model_path = paste0("src/models/", model_file, ".stan"),
+         fit = pmap(list(model_path, data, control),
+                    ~ stan(file = ..1, data = ..2,
+                           chains = 4, iter = 4000, warmup = 2000,
+                           control = ..3)))
+walk(short_df$fit, function(f) f@stanmodel@dso <- new('cxxdso'))
+save(fit_df, chain_spec,
+     file = paste0("results/", Sys.time(), "_shortfits.Rdata"))
--- src/05_results.R
+++ src/05_results.R
@@ -2,16 +2,18 @@ library(rstan)
 library(bayesplot)
 library(tidyverse)
 
-load("results/fits.Rdata")
+##-Model fits-------------------------------------------------------------------
+
+fit_datetime <- "2018-11-13 05:22:19"
+load(paste0("results/", fit_datetime, "_fits.Rdata"))
 
 post_df <- fit_df %>%
   transmute(model_name = model_name,
             post = map(fit, extract, inc_warmup = FALSE))
 
-
 diag_df <- fit_df %>%
   mutate(ess = map(fit, ~ summary(.)$summary[, "n_eff"])) %>%
-  transmute(model_name = model_name,
+  transmute(model_name = factor(model_name, unique(model_name)),
             td_total = map_dbl(fit, get_num_max_treedepth),
             div_total = map_dbl(fit, get_num_divergent),
             ## na.rm to get rid of fixed (Pmed[1]) pars with NaN ESS
@@ -21,7 +23,27 @@ diag_df <- fit_df %>%
                                   function(p) attr(p, "names")[which.min(p)]),
             max_ess = map_dbl(ess, max, na.rm = TRUE))
 
+pdiag <- diag_df %>%
+  select(model_name, td_total, div_total)
+pdiag2 <- pdiag[c(1, seq(2, 13, 2)), ]
+pdiag2$td_adj <- c(NA, pdiag$td_total[seq(3, 13, 2)])
+pdiag2$div_adj <- c(NA, pdiag$div_total[seq(3, 13, 2)])
+pdiag2 <- select(pdiag2,
+                 ` ` = model_name,
+                 `Exc Treedepth` = td_total,
+                 `Exc Treedepth Adj` = td_adj,
+                 `Divergences` = div_total,
+                 `Divergences Adj` = div_adj)
+
+present_diag_df <- diag_df %>%
+  select(` ` = model_name,
+         `edepth` = td_total,
+         `Divergences` = div_total)
+present_diag_df <- present_diag_df[c(1, seq(2, 13, 2)), ]
+present_diag_df$`Adj Max Treedepth` <- c(NA, present_diag_df$`Exc Max Treedepth`[seq(3, 13, 2)])
+
 time_df <- fit_df %>%
+  mutate(model_name = factor(model_name, unique(model_name))) %>%
   left_join(diag_df, by = "model_name") %>%
   mutate(times = map(fit, get_elapsed_time)) %>%
   transmute(model_name = model_name,
@@ -30,3 +52,51 @@ time_df <- fit_df %>%
             min_ess_rate = min_ess / sample,
             exp_ess_rate = exp_ess / sample)
 
+time_unadj <- time_df[c(1, seq(2, 13, 2)), ]
+knitr::kable(time_unadj)
+
+time_adj <- time_df[seq(3, 13, 2), ]
+knitr::kable(time_adj)
+
+##-Short series fits------------------------------------------------------------
+
+shortfit_datetime <- "2018-11-13 05:45:55"
+load(paste0("results/", shortfit_datetime, "_shortfits.Rdata"))
+
+diag_df <- short_df %>%
+  mutate(ess = map(fit, ~ summary(.)$summary[, "n_eff"])) %>%
+  transmute(model_name = factor(model_name, unique(model_name)),
+            T = T,
+            td_total = map_dbl(fit, get_num_max_treedepth),
+            div_total = map_dbl(fit, get_num_divergent),
+            ## na.rm to get rid of fixed (Pmed[1]) pars with NaN ESS
+            exp_ess = map_dbl(ess, mean, na.rm = TRUE),
+            min_ess = map_dbl(ess, min, na.rm = TRUE),
+            min_ess_par = map_chr(ess,
+                                  function(p) attr(p, "names")[which.min(p)]),
+            max_ess = map_dbl(ess, max, na.rm = TRUE))
+
+
+time_df <- short_df %>%
+  mutate(model_name = factor(model_name, unique(model_name))) %>%
+  left_join(diag_df, by = "model_name") %>%
+  mutate(times = map(fit, get_elapsed_time),
+         model_name = factor(model_name, levels = unique(diag_df$model_name))) %>%
+  transmute(model_name = model_name,
+            T = T,
+            warmup = map_dbl(times, ~ sum(.[, "warmup"])),
+            sample = map_dbl(times, ~ sum(.[, "sample"])),
+            min_ess_rate = min_ess / sample,
+            exp_ess_rate = exp_ess / sample)
+
+shortdivs <- diag_df %>%
+  select(model_name, T, div_total) %>%
+  spread(key = T, value = div_total) %>%
+  arrange(model_name) %>%
+  rename(` ` = model_name)
+
+noadj_shortdivs <- shortdivs[c(1, seq(2, 13, 2)), ]
+knitr::kable(noadj_shortdivs)
+
+adj_shortdivs <- shortdivs[seq(3, 13, 2), ]
+knitr::kable(adj_shortdivs)

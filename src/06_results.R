46b3af38a032e6e522e4c3cf0a78b7e020128a26
John Best <jkbest@gmail.com>
2018-12-09 14:32:05 -0800

    Update 06_results.R to use newest fits etc

--- src/06_results.R
+++ src/06_results.R
@@ -7,7 +7,7 @@ source("src/02_fitfns.R")
 
 ##-Model fits-------------------------------------------------------------------
 
-fit_datetime <- "2018-12-02 15:51:23"
+fit_datetime <- "2018-12-05 15:49:31"
 load(paste0("results/", fit_datetime, "_fits.Rdata"))
 
 post_df <- fit_df %>%
@@ -29,8 +29,8 @@ diag_df <- fit_df %>%
 pdiag <- diag_df %>%
   select(model_name, td_total, div_total)
 pdiag2 <- pdiag[c(1, seq(2, 10, 2)), ]
-pdiag2$td_adj <- c(NA, pdiag$td_total[seq(3, 10, 2)], NA)
-pdiag2$div_adj <- c(NA, pdiag$div_total[seq(3, 10, 2)], NA)
+pdiag2$td_adj <- c(NA, pdiag$td_total[seq(3, 11, 2)])
+pdiag2$div_adj <- c(NA, pdiag$div_total[seq(3, 11, 2)])
 pdiag2 <- select(pdiag2,
                  ` ` = model_name,
                  `Exc Treedepth` = td_total,
@@ -70,7 +70,7 @@ msy_post <- fit_df %>%
   unnest() %>% unnest()
 
 msy_post %>%
-  filter(model_name %in% c("Truncated", "Centered", "Centered adj", "Marginal q", "Marginal q adj", "Explicit F")) %>%
+  filter(model_name %in% c("Truncated", "Centered", "Centered adj", "Marginal q", "Marginal q adj", "Explicit F", "Explicit F marg q")) %>%
   ggplot(aes(x = MSY, color = model_name, fill = model_name)) +
   geom_density(alpha = 0.2, size = 2) +
   labs(title = "MSY Posteriors under different parameterizations",
@@ -204,45 +204,3 @@ msy_post %>%
   ggplot(aes(x = MSY, color = catch_sd, fill = catch_sd)) +
   geom_density(alpha = 0.2, size = 2)
 
-##-Short series fits------------------------------------------------------------
-
-## shortfit_datetime <- "2018-11-13 05:45:55"
-## load(paste0("results/", shortfit_datetime, "_shortfits.Rdata"))
-
-## diag_df <- short_df %>%
-##   mutate(ess = map(fit, ~ summary(.)$summary[, "n_eff"])) %>%
-##   transmute(model_name = factor(model_name, unique(model_name)),
-##             T = T,
-##             td_total = map_dbl(fit, get_num_max_treedepth),
-##             div_total = map_dbl(fit, get_num_divergent),
-##             ## na.rm to get rid of fixed (Pmed[1]) pars with NaN ESS
-##             exp_ess = map_dbl(ess, mean, na.rm = TRUE),
-##             min_ess = map_dbl(ess, min, na.rm = TRUE),
-##             min_ess_par = map_chr(ess,
-##                                   function(p) attr(p, "names")[which.min(p)]),
-##             max_ess = map_dbl(ess, max, na.rm = TRUE))
-
-
-## time_df <- short_df %>%
-##   mutate(model_name = factor(model_name, unique(model_name))) %>%
-##   left_join(diag_df, by = "model_name") %>%
-##   mutate(times = map(fit, get_elapsed_time),
-##          model_name = factor(model_name, levels = unique(diag_df$model_name))) %>%
-##   transmute(model_name = model_name,
-##             T = T,
-##             warmup = map_dbl(times, ~ sum(.[, "warmup"])),
-##             sample = map_dbl(times, ~ sum(.[, "sample"])),
-##             min_ess_rate = min_ess / sample,
-##             exp_ess_rate = exp_ess / sample)
-
-## shortdivs <- diag_df %>%
-##   select(model_name, T, div_total) %>%
-##   spread(key = T, value = div_total) %>%
-##   arrange(model_name) %>%
-##   rename(` ` = model_name)
-
-## noadj_shortdivs <- shortdivs[c(1, seq(2, 13, 2)), ]
-## knitr::kable(noadj_shortdivs)
-
-## adj_shortdivs <- shortdivs[seq(3, 13, 2), ]
-## knitr::kable(adj_shortdivs)

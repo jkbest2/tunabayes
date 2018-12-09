0b66c11c405be0d547c27c318ba30f8df46a9d8b
John Best <jkbest@gmail.com>
2018-12-09 14:30:02 -0800

    Move init functions to 02_fitfns.R, use in 05_exF_catchsd.R

--- src/02_fitfns.R
+++ src/02_fitfns.R
@@ -30,3 +30,33 @@ init_expF <- function(chain) {
        tau2 = 1 / rgamma(1, 1.708603, scale = 0.008613854),
        P = 1 - 0.02 * 0:(n_years - 1L) + rnorm(n_years, 0, 0.1))
 }
+
+## Retrieve a posterior sample and put in a list that can be used as initial
+## values for new fits. Need to get elements from one-dimensional parameters or
+## rows from two-dimensional parameters.
+get_post_sample <- function(iter, post_list) {
+  parnames <- names(post_list)
+  pardims <- lapply(post_list, dim)
+  samp <- lapply(parnames, function(nm) {
+    if (length(pardims[[nm]]) == 1) {
+      post_list[[nm]][iter]
+    } else {
+      post_list[[nm]][iter, 1:pardims[[nm]][2]]
+    }
+  })
+  names(samp) <- parnames
+  samp
+}
+
+get_rand_init <- function(chain_id, post_list) {
+  ## Use the total number of samples to choose from; r is a one-dimensional
+  ## parameter in all the models so it's a convenient way to get this.
+  it <- sample(1:length(post_list$r), 1L)
+  get_post_sample(it, post_list)
+}
+
+## Retrieve `n` random samples from the previously fit posterior
+make_init_list <- function(n, post_list) {
+  it <- sample(1:length(post_list$r), n)
+  lapply(it, function(i) get_post_sample(i, post_list))
+}
--- src/05_exF_catchsd.R
+++ src/05_exF_catchsd.R
@@ -3,56 +3,56 @@ source("src/01_setup.R")
 ## Load plot specifics and initial value function
 source("src/02_fitfns.R")
 
-load("results/2018-12-02 15:51:23_fits.Rdata")
-post10 <- extract(fit_df$fit[[10]])
+## Load previously fit posterior; use these samples as initial values for new
+## fits
+datetime <- "2018-12-05 15:49:31"
+load(paste0("results/", datetime, "_fits.Rdata"))
+post_exF <- rstan::extract(fit_df$fit[[8]])
 
 set.seed(9448949)
 
-chain_spec <- list(n_iter = 1e4,
+## Same specifications as other fits; 2e4 should be enough to pick up
+## pathologies and allow for comparison of posterior vales, and 5e3 warmup
+## should give plenty of time for adaptation
+chain_spec <- list(n_iter = 2e4,
                    n_warm = 5e3,
-                   n_chain = 4)
+                   n_chain = 6L)
+chain_spec <- list(n_iter = 2e3,
+                   n_warm = 5e1,
+                   n_chain = 6L)
 
 ## Explicit F flattens posterior so much that it needs a larger treedepth
 exF_control = list(max_treedepth = 15L)
 
-## Standard deviations to try for catch, from 2e-3 to 200
-catch_sd <- 10^(seq(-2, 2, length.out = 7L))
+## Standard deviations to try for catch, from 1e-2 to 100
+catch_sd <- 10^(seq(-2, 2, length.out = 10L))
 
+## Function to change the `catch_sd` of each data list
 catch_sd_data <- function(catch_sd) {
   td2 <- tuna_data
   td2$catch_sd <- catch_sd
   td2
 }
 
-get_post_sample <- function(iter, post_list = post10) {
-  parnames <- c("r", "K", "q", "F", "sigma2", "tau2", "P")
-  list(r = post_list$r[iter],
-       K = post_list$K[iter],
-       q = post_list$q[iter],
-       F = post_list$F[iter, ],
-       sigma2 = post_list$sigma2[iter],
-       tau2 = post_list$tau2[iter],
-       P = post_list$P[iter, ])
-}
-
-get_rand_post <- function(chain) {
-  it <- sample(1:20000, 1L)
-  get_post_sample(it, post10)
-}
-
 ## Model location
 modfile <- "src/models/30_exF.stan"
 
+## Retrieve initialization values for each chain using previous model fit
+init_exF <- make_init_list(chain_spec$n_chain, post_exF)
+
+## Fit the models in a data frame
 csd_df <- data_frame(catch_sd = catch_sd) %>%
   mutate(data = map(catch_sd, catch_sd_data),
          fit = map(data, ~ stan(file = modfile,
                                 data = .x,
-                                init = get_rand_post,
+                                init = init_exF,
                                 chains = chain_spec$n_chain,
                                 iter = chain_spec$n_iter,
                                 warmup = chain_spec$n_warm,
                                 control = list(max_treedepth = 15L))))
+## Do this so that the models can be serialized and then reloaded successfully
 walk(csd_df$fit, function(f) f@stanmodel@dso <- new('cxxdso'))
+## Save with a unique filename
 save(csd_df, chain_spec,
      file = paste0("results/", Sys.time(), "_csd.Rdata"))
 

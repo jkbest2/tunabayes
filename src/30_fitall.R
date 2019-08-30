4bc1e3bea2a44591366e6b20c0d7b2f031e53411
John Best <jkbest@gmail.com>
2019-08-30 14:00:21 -0700

    Move full PT fits to own script

--- src/30_fitall.R
+++ src/30_fitall.R
@@ -20,7 +20,6 @@ source("src/10_data.R")
 ## than 0.05, which looks reasonable in prior predictive checks.
 tuna_data$catch_cv_prior_rate <- cv_prior_rate(0.05, 0.9)
 
-
 ## Set the specifications for each chain. 25,000 total iterations with 5,000
 ## warmup iterations and 6 chains. Overkill, but better for detecting any sneaky
 ## divergent transitions.
@@ -31,46 +30,5 @@ chain_spec <- list(n_iter = 2.5e4,
 ## Choose a range of `adapt_delta` values (default is 0.8)
 adapt_delta_values <- c(seq(0.7, 0.95, 0.05), 0.975, 0.99, 0.999)
 
-## In order to avoid needing to recompile the model every run, first compile
-## each model in global scope.
-## <https://discourse.mc-stan.org/t/stan-recompile-to-avoid-r-from-crashing/2631/2>
-mod_centered <- stan_model("src/models/01_centered.stan")
-mod_ncproc <- stan_model("src/models/10_ncproc.stan")
-mod_margq <- stan_model("src/models/20_margq.stan")
-mod_exF <- stan_model("src/models/30_exF.stan")
-mod_exF_margq <- stan_model("src/models/32_exF_margq.stan")
-
-## Set up a data frame with each compiled model, its name, and initial value
-## generator. This is used to cross with the `adapt_delta_values` vector to get
-## all combinations.
-model_df <- tribble(
-  ~ model_name       , ~ model      ,      ~ init,
-  "Centered"         ,  mod_centered,   init_cent,
-  "Noncentered"      ,    mod_ncproc, init_ncproc,
-  "Marginal q"       ,     mod_margq,   init_cent,
-  "Explicit F"       ,       mod_exF,   init_expF,
-  "Explicit F marg q", mod_exF_margq,   init_expF)
-
-chain_spec <- list(n_iter = 2000,
-                   n_warm = 1000,
-                   n_chain = 2L)
-
-## Sample from each of these models
-fit_df <- cross_df(list(model_name = model_df$model_name,
-                        adapt_delta = adapt_delta_values)) %>%
-  left_join(model_df, by = "model_name") %>%
-  #########################################################################
-  ## FIXME Only use next line when debugging! Doesn't fit all combinations!
-  #########################################################################
-  ## filter(adapt_delta == 0.80) %>%
-  mutate(fit = pmap(., ~ sampling(object = ..3,
-                                  data = tuna_data,
-                                  chains = chain_spec$n_chain,
-                                  iter = chain_spec$n_iter,
-                                  warmup = chain_spec$n_warm,
-                                  init = ..4,
-                                  control = list(adapt_delta = ..2))))
-
-save(fit_df, chain_spec,
-     file = "results/adapt_delta_fits.Rdata")
+source("src/31_fitfullPT.R")
 
---
+++ src/31_fitfullPT.R
@@ -0,0 +1,34 @@
+## In order to avoid needing to recompile the model every run, first compile
+## each model in global scope.
+## <https://discourse.mc-stan.org/t/stan-recompile-to-avoid-r-from-crashing/2631/2>
+fullPT_centered <- stan_model("src/models/01_centered.stan")
+fullPT_ncproc <- stan_model("src/models/10_ncproc.stan")
+fullPT_margq <- stan_model("src/models/20_margq.stan")
+fullPT_exF <- stan_model("src/models/30_exF.stan")
+fullPT_exF_margq <- stan_model("src/models/32_exF_margq.stan")
+
+## Set up a data frame with each compiled model, its name, and initial value
+## generator. This is used to cross with the `adapt_delta_values` vector to get
+## all combinations.
+fullPT_models <- tribble(
+  ~ model_name       ,  ~ model        ,      ~ init,
+  "Centered"         ,  fullPT_centered,   init_cent,
+  "Noncentered"      ,    fullPT_ncproc, init_ncproc,
+  "Marginal q"       ,     fullPT_margq,   init_cent,
+  "Explicit F"       ,       fullPT_exF,    init_exF,
+  "Explicit F marg q", fullPT_exF_margq,    init_exF)
+
+## Sample from each of these models
+fullPT_fits <- cross_df(list(model_name = fullPT_models$model_name,
+                        adapt_delta = adapt_delta_values)) %>%
+  left_join(fullPT_models, by = "model_name") %>%
+  mutate(fit = pmap(., ~ sampling(object = ..3,
+                                  data = tuna_data,
+                                  chains = chain_spec$n_chain,
+                                  iter = chain_spec$n_iter,
+                                  warmup = chain_spec$n_warm,
+                                  init = ..4,
+                                  control = list(adapt_delta = ..2))))
+
+save(fullPT_fits, chain_spec,
+     file = "results/fullPT_fits.Rdata")

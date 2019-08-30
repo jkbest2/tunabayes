20e31d562b78f9214d031289207c375a425af10d
John Best <jkbest@gmail.com>
2019-08-30 13:49:56 -0700

    Move package loading to fitting script where it's needed

--- src/10_data.R
+++ src/10_data.R
@@ -1,18 +1,6 @@
-## Data wrangling and map workflow
-library(tidyverse)
-## Use the `futures` package to distribute model fits
-## library(furrr)
-## plan(multicore)
-## RStan
-library(rstan)
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-## Required for between-chain parallelization for unknown reason (see rstan
-## issue 556 on Github)
-library(later)
-library(bayesplot)
-
-# data: catches, CPUE Index, and number of years
+# Data: catches, CPUE Index, and number of years. Taken from Meyer and Millar
+# (1999), but also available in Polachek et al. (1993). Depending on the model
+# to be fit, other information may be added.
 catch <- c(15.9, 25.7, 28.5, 23.7, 25.0, 33.3, 28.2, 19.7, 17.5, 19.3, 21.6,
            23.1, 22.5, 22.5, 23.6, 29.1, 14.4, 13.2, 28.4, 34.6, 37.5, 25.9,
            25.3)
@@ -20,10 +8,8 @@ indices <- c(61.89, 78.98, 55.59, 44.61, 56.89, 38.27, 33.84, 36.13, 41.95,
              36.63, 36.33, 38.82, 34.32, 37.64, 34.01, 32.16, 26.88, 36.61,
              30.07, 30.75, 23.36, 22.36, 21.91)
 n_years <- 23L
-catch_sd <- 0.2
 
 tuna_data <- list(C = catch,
                   I = indices,
-                  T = n_years,
-                  catch_sd = catch_sd)
+                  T = n_years)
 
--- src/30_fitall.R
+++ src/30_fitall.R
@@ -1,24 +1,35 @@
-## Load packages and data
-source("src/01_setup.R")
-## Load plot specifics and initial value function
-source("src/02_fitfns.R")
+## Data wrangling and map workflow
+library(tidyverse)
+## RStan
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+## Required for between-chain parallelization for unknown reason (see rstan
+## issue 556 on Github)
+library(later)
+
+## Don't set a seed; results shouldn't depend on it!
+
+## Source helper functions
+source("src/01_functions.R")
+## Read in data
+source("src/10_data.R")
 
 ## Add the `catch_cv_prior_rate` to `tuna_data`; required for the "explicit F"
 ## parameterizations. This is set to use a 90% probability that the CV is less
 ## than 0.05, which looks reasonable in prior predictive checks.
 tuna_data$catch_cv_prior_rate <- cv_prior_rate(0.05, 0.9)
 
-set.seed(38493)
 
-## Set the specifications for each chain. Currently 20,000 total iterations,
-## with 5,000 warmup iterations and 6 chains. Overkill, but better for detecting
-## any sneaky divergent transitions.
-chain_spec <- list(n_iter = 2e4,
+## Set the specifications for each chain. 25,000 total iterations with 5,000
+## warmup iterations and 6 chains. Overkill, but better for detecting any sneaky
+## divergent transitions.
+chain_spec <- list(n_iter = 2.5e4,
                    n_warm = 5e3,
                    n_chain = 6L)
 
 ## Choose a range of `adapt_delta` values (default is 0.8)
-adapt_delta_values <- c(seq(0.75, 0.95, 0.05), 0.975, 0.99)
+adapt_delta_values <- c(seq(0.7, 0.95, 0.05), 0.975, 0.99, 0.999)
 
 ## In order to avoid needing to recompile the model every run, first compile
 ## each model in global scope.

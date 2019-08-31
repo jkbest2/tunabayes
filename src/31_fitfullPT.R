dc11a155b85931c0382e0aecce12d9b104bd79df
John Best <jkbest@gmail.com>
2019-08-31 12:45:32 -0700

    Renumber full PT models for better sorting

--- src/31_fitfullPT.R
+++ src/31_fitfullPT.R
@@ -4,11 +4,11 @@ source("src/30_fitall.R")
 ## In order to avoid needing to recompile the model every run, first compile
 ## each model in global scope.
 ## <https://discourse.mc-stan.org/t/stan-recompile-to-avoid-r-from-crashing/2631/2>
-fullPT_centered <- stan_model("src/models/01_centered.stan")
-fullPT_ncproc <- stan_model("src/models/10_ncproc.stan")
-fullPT_margq <- stan_model("src/models/20_margq.stan")
-fullPT_exF <- stan_model("src/models/30_exF.stan")
-fullPT_exF_margq <- stan_model("src/models/32_exF_margq.stan")
+fullPT_centered <- stan_model("src/models/001_centered.stan")
+fullPT_ncproc <- stan_model("src/models/010_ncproc.stan")
+fullPT_margq <- stan_model("src/models/020_margq.stan")
+fullPT_exF <- stan_model("src/models/030_exF.stan")
+fullPT_exF_margq <- stan_model("src/models/032_exF_margq.stan")
 
 ## Set up a data frame with each compiled model, its name, and initial value
 ## generator. This is used to cross with the `adapt_delta_values` vector to get
